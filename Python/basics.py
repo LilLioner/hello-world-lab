785eca1164dd6587d199d022e1a01903134b7ab3
Lioner <leonardorcruz2000@gmail.com>
2026-02-02 13:02:36 -0300

    New Python Basics Folder

---
+++ Python/basics.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+
+a = 1 
+b = 2 
+c = a+b
+print(c)
+print(a+b)
+
+###################################
+
+for i in range(5):
+  print("Hello World!")
+  
+###################################
+
+## Calculadora de Média entre 4 numeros
+
+q = float(input("Numero 1 ="))
+w = float(input("Numero 2 ="))
+e = float(input("Numero 3 ="))
+r = float(input("Numero 4 ="))
+
+print("A média dos quatro numeros acima é : ", (q+w+e+r)/4)
+
+###################################
