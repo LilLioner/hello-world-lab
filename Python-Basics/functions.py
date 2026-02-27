c5df522950fb2b7676993ac390ddb51da23e9e5b
Lioner <leonardorcruz2000@gmail.com>
2026-02-27 13:19:25 -0300

    new exercise in functions.py

--- Python-Basics/functions.py
+++ Python-Basics/functions.py
@@ -1,16 +1,28 @@
-<<<<<<< HEAD:Python/functions.py
+'''
 def eh_par(x):
-  if x % 2:
-    return False
-  else:
-    return True
+  return x %2 == 0
   
 print(eh_par(3))
-=======
+
+#############
 def dobro(a):
   return a*2
 
 valor = dobro(float(input("Escolha um número para ser duplicado : ")))
 
-print(valor)
->>>>>>> 918d8d7867cbfea1abf3c513adfe82ec899d1aa2:Python-Basics/functions.py
+print(valor)'''
+
+def analisar_numeros(lista):
+  total = 0
+  pares = []
+  impares = []
+  for n in lista:
+    total += n
+    if n % 2 == 0 :
+      pares.append(n)
+    else:
+      impares.append(n)
+  media = total / (len(lista))
+  return pares, impares, total, media
+
+print(analisar_numeros([1,2,3,4]))
