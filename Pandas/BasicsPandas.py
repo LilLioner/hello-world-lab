5793f8627cd5b81443d5436510313ce11b43f67d
Lioner <leonardorcruz2000@gmail.com>
2026-03-13 12:25:53 -0300

    Pandas learning upds

--- Pandas/BasicsPandas.py
+++ Pandas/BasicsPandas.py
@@ -1,11 +1,32 @@
 from pathlib import Path
 import pandas as pd 
-
 base = Path(__file__).parent
 
-df = pd.read_csv(base / "data.csv")
+####################################
 
-print(df)
-print(df.head())
-print(df.info())
-print(df.describe())
+
+
+df = { 
+  "nome" : ["Ana", "João", "Maria"], 
+  "idade" : [25, 30, 28], 
+  "salario" : [3000, 5000, 4000] 
+  } 
+
+df = pd.DataFrame(df)
+
+print(df[(df["idade"] > 28) & (df["salario"] > 3500)]) #FILTRANDO DATA FRAMES
+print(df["salario"].describe())
+
+
+print(df.head(2)) 
+print()
+print()
+print()
+
+print(df.loc[df["idade"] > 25, ["nome","salario"]])
+
+####################################
+
+var = pd.read_csv(base / "data.csv")
+
+
